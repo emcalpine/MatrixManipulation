233fb0aaf8bf55d24e63613ce61008340a7d0742
Eric McAlpine <ewm1001@sru.edu>
2015-11-10 21:54:12 -0500

    Finished main functionality
    Check it out

--- MatrixMult/MatrixMult/MatrixRowCmd.cpp
+++ MatrixMult/MatrixMult/MatrixRowCmd.cpp
@@ -5,38 +5,30 @@
 
 using namespace std;
 
+const float ERROR = 0.000005;
+
 void display(Matrix m);
+void newMat(Matrix* &a, int i);
+void addOneMat(Matrix* &a, int n);
 
 int main()
 {
 
-	Matrix myMat;
+	Matrix* ary;
 	string control;
 	bool done = false;
-	int rows;
-	int cols;
+	int curMat = 0;
+	int numMats = 1;
 
-	//init matrix;
-	cout << "rows:" << endl;
-	cin >> rows;
-	cout << "cols:" << endl;
-	cin >> cols;
+	//init array
+	ary = new Matrix[numMats];
+	cout << "Starting with 1 matrix\n";
+	newMat(ary, curMat);	//init first matrix
 
-	myMat = Matrix(rows, cols);
-
-	for (int r = 0; r < rows; r++)
-	{
-		for (int c = 0; c < cols; c++)
-		{
-			float val;
-			cin >> val;
-			myMat.setNumber(r, c, val);
-		}
-	}
 
 	while (!done)
 	{
-		display(myMat);
+		display(ary[curMat]);
 
 		cout << endl;
 		getline(cin, control);
@@ -50,6 +42,9 @@ int main()
 		float arg1;
 		float arg2;
 		float arg3 = -1.0;
+		//************************
+		//	Parse command
+		//************************
 		for (unsigned int i = 0; i < control.length(); i++)
 		{
 			if (control.at(i) != ' ' && control.at(i) != '\n')
@@ -75,16 +70,56 @@ int main()
 				curArgVal = "";
 			}
 		}
+
+		//****************************
+		//	Operation switch
+		//****************************
 		if (operation == "swap")
 		{
-			myMat.rowSwap(arg1, arg2);
+			ary[curMat].rowSwap(arg1, arg2);
 		}
-		else if (operation == "multiply")
+		else if (operation == "rm")
 		{
 			if (arg3 == -1)
-				myMat.rowMult(arg1, arg2);
+				ary[curMat].rowMult(arg1, arg2);
 			else
-				myMat.rowMult(arg1, arg2, arg3);
+				ary[curMat].rowMult(arg1, arg2, arg3);
+		}
+		else if (operation == "mult")
+		{
+			//bounds check for 2 source matricies
+			if (arg1 >= 0 && arg1 < numMats && arg2 >= 0 && arg2 < numMats)
+			{
+				//check bounds for dest
+				if (arg3 >= 0 && arg3 < numMats)
+					ary[(int)arg3] = ary[(int)arg1].smartMultiply(ary[(int)arg2]);
+				else if (abs(arg3-numMats) < ERROR)
+				{
+					addOneMat(ary, numMats++);
+					ary[(int)arg3] = ary[(int)arg1].smartMultiply(ary[(int)arg2]);
+				}
+				else
+					cout << "\n\n***** Invalid desitnation index *****\n\n";
+			}
+		}
+		else if (operation == "create")
+		{
+			//bounds check
+			if (arg1 >= 0 && arg1 < numMats)
+			{
+				newMat(ary, (int)arg1);
+			}
+			else if (abs(arg1-numMats) < ERROR)	//arg1 == numMats
+			{
+				addOneMat(ary, numMats);
+				newMat(ary, numMats);
+				numMats++;
+			}
+		}
+		else if (operation == "switch")
+		{
+			//switch to valid index, else stay same
+			curMat = (arg1 >= 0 && arg1 < numMats) ? arg1 : curMat;
 		}
 		else if (operation == "quit")
 		{
@@ -95,13 +130,19 @@ int main()
 			cout << "\n syntax: \n [operation] [arg1] [arg2] [arg3]\n";
 			cout << "opeartions:\n";
 			cout << "\tswap:\n\t\trow1\n\t\trow2\n";
-			cout << "\tmultiply: (m*r1) / r2+(m*r1)\n\t\tmultiplier\n\t\trow1\n\t\t[row2]\n";
+			cout << "\trm: (m*r1) / r2+(m*r1)\n\t\tmultiplier\n\t\trow1\n\t\t[row2]\n";
+			cout << "\tmult\n\t\tmatrix 1\n\t\tmatrix2\n\t\tdestination index\n";
+			cout << "\tcreate\n\t\tindex\n";
+			cout << "\tswitch\n\t\tindex\n";
 			cout << "\tquit\n\n";
 		}
 
 	}
+
+	delete[] ary;
 }
 
+// Print out matrix
 void display(Matrix m)
 {
 	for (int i = 0; i < m.getNumRows(); i++)
@@ -113,4 +154,48 @@ void display(Matrix m)
 		}
 		cout << "|" << endl;
 	}
+}
+
+//**************************
+//	create new matrix at index i
+//**************************
+void newMat(Matrix* &a, int i)
+{
+	int rows;
+	int cols;
+
+	cout << "rows:" << endl;
+	cin >> rows;
+	cout << "cols:" << endl;
+	cin >> cols;
+
+	//error checking
+	rows = (rows >0) ? rows : 1;
+	cols = (cols > 0) ? cols : 1;
+
+	a[i] = Matrix(rows, cols);
+
+	//init values
+	for (int r = 0; r < rows; r++)
+	{
+		for (int c = 0; c < cols; c++)
+		{
+			float val;
+			cin >> val;
+			a[i].setNumber(r, c, val);
+		}
+	}
+
+}
+
+//<param> n current size
+void addOneMat(Matrix* &a, int n)
+{
+	Matrix* temp = new Matrix[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		temp[i] = a[i];
+	}
+	delete[] a;
+	a = temp;
 }
