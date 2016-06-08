0151959aa1af1974b1ef6d2e225f3e51d681b732
Eric McAlpine <emcalpine144@gmail.com>
2016-06-08 10:47:26 -0400

    Create Fraction.h

---
+++ MatrixMult/MatrixMult/Fraction.h
@@ -0,0 +1,44 @@
+/*
+ *  Eric McAlpine
+ *  Fraction.h
+ *
+ * 
+ *  Purpose: Define a fraction 
+ *
+ *
+ */
+ 
+ #ifndef FRACTION_H
+ #define FRACTION_H
+ 
+class Fraction
+{
+public:
+  Fraction();
+  Fraction(int n, int d);
+  Fraction(Fraction f);
+  
+  bool operator==(Fraction f);
+  Fraction operator=(Fraction f);
+  Fraction operator+(Fraction f);
+  Fraction operator-(Fraction f);
+  Fraction operator*(Fraction f);
+  Fraction operator/(Fraction f);
+  
+  bool setNumerator(int n);
+  bool setDenominator(int d);
+  int getNumerator();
+  int getDenominator();
+  
+  string toString();
+
+private:
+  int denominator;
+  int numerator;
+  void reduce();
+  
+  static const int DEFAULT_NUM;
+  static const int DEFAULT_DEN;
+};
+
+#endif
